57aef8ee1d2653910b17f53d74e93a83d2500fd8
EsslamAshour <2023.esslam.ashour@uwcisak.jp>
2021-09-26 23:52:59 +0200

    db logic

--- YuushaNoTsurugi/game.py
+++ YuushaNoTsurugi/game.py
@@ -3,18 +3,11 @@ import time
 import os
 import sys
 
-# TODO: add data for another 9 levels, move it to a separate file
-# Possibly use dictionary comprehensions?
-level_data = {
-    1: {
-        "name": "FLAMMA",
-        "hp": 15,
-        "strength": 5,
-        "block_chance": 1
-    }
-}
+import db
 
-score = 0
+
+level_data = db.level_data
+name = None
 current_lvl = 0
 
 def start():
@@ -27,75 +20,77 @@ def start():
     answer = input(f">> Gladiator {name}, are you ready? (y/n) ").lower()
     answer = validate(answer, ["y", "n"])
     if answer == "y":
+        db.insert_player((name, 0))
         clear()
-        start_time = time.time()
         lvl_start(1)
     else:
         print("Coward.")
         sys.exit()
 
-# TODO: Add block for enemy, organize code, more fighting options, make it so the program doesn't print the attack log when one hp reaches 0.
 def lvl_start(lvl):
-    enemy_name = level_data[lvl]["name"]
-    player_hp = 15
-    enemy_hp = level_data[lvl]["hp"]
-    blocked = False
+    enemy_name = level_data[lvl]["enemy_name"]
+    enemy_hp = level_data[lvl]["enemy_hp"]
+    enemy_strength = 232323 #level_data[lvl]["enemy_strength"]
+    player_hp = level_data[lvl]["player_hp"]
+    player_strength = level_data[lvl]["player_strength"]
     winner = None
+    start_time = time.time()
+    
     print(">> FIGHT START <<")
-    time.sleep(1)
+    time.sleep(0.7)
     print(f">> LEVEL {lvl} - YOUR HP: {player_hp} - STRENGTH 5 <<")
     print(f">> GLADIATOR {enemy_name} - {enemy_hp} HP - STRENGTH 5 <<")
-   
+    
     while player_hp > 0 and enemy_hp > 0:
         # Player's turn
-        choice = input("""
-        >> What will you do?
-            > Attack (a)
-            > Block (b) 
+        atk_prompt = input("""
+        >> PRESS a TO ATTACK
         """).lower()
-        choice = validate(choice, ["a", "b"])  
-        if choice == "a":
-            player_atk = random.randint(1, 5)
-            print(f"YOU ATTACKED {enemy_name}")
-            time.sleep(1)
-            if blocked:
-                print(f"YOU TRIED TO ATTACK {enemy_name}, BUT {enemy_name} BLOCKS THE ATTACK!")
-            else:
-                print(f"YOUR ATTACK DEALT {player_atk} DAMAGE. {enemy_name}'S HP WAS REDUCED FROM {enemy_hp} TO {enemy_hp-player_atk}")
-                print("======================================================")
-                enemy_hp -= player_atk
-                if enemy_hp <= 0:
-                    winner = "player"
-                    break
-            time.sleep(1)
-        else:
-            
-            block_chance = random.choices([True, False], [0.1, 0.9])           
-        if blocked:
-            print(f"{enemy_name} TRIED TO ATTACK, BUT YOU BLOCK THE ATTACK!")
-            blocked = False
-            print("======================================================")
-        # Enemy's turn
-        else:
-            enemy_strength = level_data[lvl]["strength"]
-            enemy_atk = random.randint(1, enemy_strength)
-            print(f"{enemy_name} ATTACKED YOU")
-            time.sleep(1)
-            print(f"{enemy_name}'S ATTACK DEALT {enemy_atk} DAMAGE. YOUR HP WAS REDUCED FROM {player_hp} to {player_hp-enemy_atk}")
-            print("======================================================")
-            player_hp -= enemy_atk
-            if player_hp <= 0:
-                winner = "enemy"
-                break
+        atk_prompt = validate(atk_prompt, ["a"])  
+        player_atk = random.randint(1, player_strength)
+        print(f"YOU ATTACKED {enemy_name}")
+        time.sleep(0.7)
+        print(f"YOUR ATTACK DEALT {player_atk} DAMAGE. {enemy_name}'S HP WAS REDUCED FROM {enemy_hp} TO {enemy_hp-player_atk}\n")
+        enemy_hp -= player_atk
+        if enemy_hp <= 0:
+            winner = "player"
+            break
+        time.sleep(1)
+        
+        # Enemy turn
+        enemy_atk = random.randint(1, enemy_strength)
+        print(f"{enemy_name} ATTACKED YOU")
+        time.sleep(0.7)
+        print(f"{enemy_name}'S ATTACK DEALT {enemy_atk} DAMAGE. YOUR HP WAS REDUCED FROM {player_hp} to {player_hp-enemy_atk}\n")        
+        player_hp -= enemy_atk
+        if player_hp <= 0:
+            winner = "enemy"
+            break
                 
     if winner == "player":
-        print("YOU WON!")
+        end_time = time.time()
+        play_time = start_time - end_time
+        print(">> YOU WON! <<")
+        if lvl == 10:
+            print(">> CONGRATULATIONS. YOU ARE THE CHAMPION OF THE COLLOSSEUM! - TIME PLAYED: {play_time} <<")
+            db.update_score(name)
+            sys.exit()
+        db.update_score(name)
+        print(f"NEXT LEVEL: {lvl+1} - STARTING IN 3 SECONDS")
+        time.sleep(3)
+        clear()
+        lvl_start(lvl+1)
     else:
+        end_time = time.time()
+        play_time = end_time - start_time
         print("YOU DIED.")
-        print(">> GAME OVER <<")
+        print(f">> GAME OVER - SCORE {db.get_score(name)} - TIME PLAYED: {play_time}<<")
 
 def get_name():
     name = input(">> What's your name, gladiator? (3-12 characters) ")
+    existing_names = db.get_existing_names()
+    while name in existing_names:
+        name = input(">> Name already exists, please try another name: ")
     while len(name) < 3 or len(name) > 12:
         long_or_short = "short" if len(name) < 3 else "long"
         name = input(f">> Name too {long_or_short}. Please try a longer name. (3-12 characters)")
